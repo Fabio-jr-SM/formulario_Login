e7c013becd59740f970a9a07a2049ca34e41baf8
FlutLab.io <>
2022-07-05 15:59:22 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -14,65 +14,65 @@ class LoginPage extends StatelessWidget {
       body: _body(),
     );
   }
-}
 
-_body() {
-  return Container(
-    padding: EdgeInsets.all(16),
-    child: ListView(
-      children: <Widget>[
-        _text("Login", "Digite o Login", controller: _tLogin),
-        SizedBox(height: 20),
-        _text("Senha", "Digite o Senha", password: true, controller: _tSenha),
-        SizedBox(height: 20),
-        _button("Login", _onClickLogin),
-      ],
-    ),
-  );
-}
+  _body() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          _text("Login", "Digite o Login", controller: _tLogin),
+          SizedBox(height: 20),
+          _text("Senha", "Digite o Senha", password: true, controller: _tSenha),
+          SizedBox(height: 20),
+          _button("Login", _onClickLogin),
+        ],
+      ),
+    );
+  }
 
-_text(String label, String hint, {bool password = false, TextEditingController controller}) {
-  return TextFormField(
-    controller: controller,
-    obscureText: password,
-    style: TextStyle(
-      fontSize: 20,
-      color: Colors.blue,
-    ),
-    decoration: InputDecoration(
-      labelText: label,
-      labelStyle: TextStyle(
+  _text(String label, String hint, {bool password = false, TextEditingController controller}) {
+    return TextFormField(
+      controller: controller,
+      obscureText: password,
+      style: TextStyle(
         fontSize: 20,
         color: Colors.blue,
       ),
-      hintText: hint,
-      hintStyle: TextStyle(
-        fontSize: 16,
-      ),
-    ),
-  );
-}
-
-_button(String text, Function onPressed) {
-  return Container(
-    height: 46,
-    child: RaisedButton(
-      color: Colors.blue,
-      child: Text(
-        text,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 22,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(
+          fontSize: 20,
+          color: Colors.blue,
+        ),
+        hintText: hint,
+        hintStyle: TextStyle(
+          fontSize: 16,
         ),
       ),
-      onPressed: onPressed,
-    ),
-  );
-}
+    );
+  }
 
-void _onClickLogin() {
-  String login = _tLogin.text;
-  String senha = _tSenha.text;
+  _button(String text, Function onPressed) {
+    return Container(
+      height: 46,
+      child: RaisedButton(
+        color: Colors.blue,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
 
-  print("Login: $login, Senha: $senha");
+  void _onClickLogin() {
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    print("Login: $login, Senha: $senha");
+  }
 }
