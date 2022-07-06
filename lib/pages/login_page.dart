8e69a76130f5d1da4438e104f9487f83a2b20ff7
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2022-07-06 12:00:17 -0400

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,70 +1,90 @@
 import 'package:flutter/material.dart';
 
+// ignore: use_key_in_widget_constructors
 class LoginPage extends StatelessWidget {
-
+  final _tLogin = TextEditingController();
+  final _tSenha = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // ignore: prefer_const_constructors
         title: Text("Carros"),
         centerTitle: true,
       ),
       body: _body(),
     );
   }
-}
 
-_body() {
-  return Container(
-    padding: EdgeInsets.all(16),
-    child: ListView(
-      children: <Widget>[
-        _text("Login", "Digite o Login"),
-        SizedBox(height: 20),
-        _text("Senha", "Digite o Senha", password: true),
-        SizedBox(height: 20),
-        _button("Login"),
-      ],
-    ),
-  );
-}
+  _body() {
+    return Container(
+      // ignore: prefer_const_constructors
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          _text("Login", "Digite o Login", controller: _tLogin),
+          // ignore: prefer_const_constructors
+          SizedBox(height: 20),
+          _text("Senha", "Digite o Senha", password: true, controller: _tSenha),
+          // ignore: prefer_const_constructors
+          SizedBox(height: 20),
+          _button("Login", _onClickLogin),
+        ],
+      ),
+    );
+  }
 
-_text(String label, String hint, {bool password = false}) {
-  return TextFormField(
-    obscureText: password,
-    style: TextStyle(
-      fontSize: 20,
-      color: Colors.blue,
-    ),
-    decoration: InputDecoration(
-      labelText: label,
-      labelStyle: TextStyle(
+  _text(String label, String hint, {bool password = false, required TextEditingController controller}) {
+    return TextFormField(
+      controller: controller,
+      obscureText: password,
+      // ignore: prefer_const_constructors
+      style: TextStyle(
         fontSize: 20,
         color: Colors.blue,
       ),
-      hintText: hint,
-      hintStyle: TextStyle(
-        fontSize: 16,
-      ),
-    ),
-  );
-}
-
-_button(String text) {
-  return Container(
-    height: 46,
-    child: RaisedButton(
-      color: Colors.blue,
-      child: Text(
-        text,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 22,
+      decoration: InputDecoration(
+        labelText: label,
+        // ignore: prefer_const_constructors
+        labelStyle: TextStyle(
+          fontSize: 20,
+          color: Colors.blue,
+        ),
+        hintText: hint,
+        // ignore: prefer_const_constructors
+        hintStyle: TextStyle(
+          fontSize: 16,
         ),
       ),
-      onPressed: () {},
-    ),
-  );
-}
+    );
+  }
 
+  _button(String text, onPressed) {
+    // ignore: sized_box_for_whitespace
+    return Container(
+      height: 46,
+      // ignore: deprecated_member_use
+      child: RaisedButton(
+        color: Colors.blue,
+        child: Text(
+          text,
+          // ignore: prefer_const_constructors
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+
+  void _onClickLogin() {
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    // ignore: avoid_print
+    print("Login: $login, Senha: $senha");
+  }
+}
